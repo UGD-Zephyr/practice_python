b94e3a7fe31bc372b1468671c15b4df8372b4ac8
Zephyr <ugd.zephyr@gmail.com>
2022-10-12 16:35:03 +0200

    updated exercise 01

--- 01_character_input.py
+++ 01_character_input.py
@@ -10,10 +10,13 @@ def hundredYearBirthday(name, age, currentYear, ageToHundred, hundredYear):
     hundred years old.
     """
 
+    print("----------------------------")
     print("Your name is", name, end=" ")
     print("and your age is", age)
     print("You will be a hundred years old in the year", hundredYear)
+    print("----------------------------")
 
+pass
 
 name            = input("name:")
 age             = int(input("age:"))
@@ -21,10 +24,16 @@ currentYear     = 2022
 ageToHundred    = 100 - age 
 hundredYear     = ageToHundred + currentYear 
 loopCounter1    = int(0)
+yearlyBirthday = input("Did you already celebrate your birhtday this year? y/n: ")
+
+if yearlyBirthday == "n" or yearlyBirthday == "N" or yearlyBirthday == "no" or yearlyBirthday == "NO" or  yearlyBirthday == "No":
+    hundredYear = (hundredYear - 1)
 
 hundredYearBirthday(name, age, currentYear, ageToHundred, hundredYear)
 
 amountOfCopies = int(input("amount of copies:"))
 
-for loopCounter1 in amountOfCopies:
+while (amountOfCopies > 0):
+
     hundredYearBirthday(name, age, currentYear, ageToHundred, hundredYear)
+    amountOfCopies = (amountOfCopies - 1)
